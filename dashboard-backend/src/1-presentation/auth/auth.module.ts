746f2fb370aba08b8a18c9c3ab4ad34223a7a308
HatimBenzahra <benzahrahatim2001@gmail.com>
2025-06-18 10:39:54 +0200

    feat(auth): load secret from env

--- dashboard-backend/src/1-presentation/auth/auth.module.ts
+++ dashboard-backend/src/1-presentation/auth/auth.module.ts
@@ -1,19 +1,24 @@
 // src/1-presentation/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { LoginUseCase } from 'src/2-application/use-cases/auth/login.use-case';
 import { DatabaseModule } from 'src/4-infrastructure/database/database.module';
 
 @Module({
   imports: [
+    ConfigModule,
     // On importe le module Database pour avoir accès au IUserRepository
     DatabaseModule,
     // On configure le module JWT
-    JwtModule.register({
+    JwtModule.registerAsync({
       global: true, // Rend le JwtService disponible dans toute l'application
-      secret: 'VOTRE_SECRET_JWT_TRES_COMPLIQUE', // !! À METTRE DANS .env EN PRODUCTION !!
-      signOptions: { expiresIn: '8h' }, // Le token expirera après 8 heures
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '8h' }, // Le token expirera après 8 heures
+      }),
     }),
   ],
   controllers: [AuthController],
